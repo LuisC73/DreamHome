e0f92fbf73bbb8450fd9a31d01583a49f4a2650a
Luis Miguel <luiscastroc11@gmail.com>
2021-11-25 16:00:13 -0500

    Primera version del index y registro

---
+++ DreamHome/Controllers/housesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DreamHome.Controllers
+{
+    public class housesController : Controller
+    {
+        // GET: houses
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RegisterHouses()
+        {
+            ViewBag.show = "houses";
+            return View();
+        }
+
+        public ActionResult EditHouses()
+        {
+            ViewBag.show = "houses";
+            return View();
+        }
+
+        public ActionResult DeleteHouses()
+        {
+            ViewBag.show = "houses";
+            return View();
+        }
+
+        public ActionResult SelectHouses()
+        {
+            ViewBag.show = "houses";
+            return View();
+        }
+    }
+}
